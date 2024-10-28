df155e80db4fb7f2150f347cbbef0758247ad834
bing1100 <bingxuhu@gmail.com>
2024-10-28 11:44:01 -0400

    updated

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from torch.profiler import record_function
 from inspect import isfunction
-
+from copy import deepcopy
 
 def normalize_to_neg_one_to_one(img):
     return img * 2 - 1
@@ -216,3 +216,123 @@ class FoundNANsError(BaseException):
     """Found NANs during sampling"""
     def __init__(self, message='Found NANs during sampling.'):
         super(FoundNANsError, self).__init__(message)
+
+def m(v):
+    nonnull = v[np.isnan(v) == False]
+    return np.mean(nonnull)
+
+def std(v):
+    nonnull = v[np.isnan(v) == False]
+    return np.std(nonnull)
+
+def infill_null(v):
+    v[np.isnan(v)] = 0
+    return v
+
+def remove_outliers(lists):
+    b = np.ones(lists[0].shape)
+
+    for l in lists:
+        q1 = np.nanquantile(l,0.25)
+        q3 = np.nanquantile(l,0.75)
+
+        iqr = q3 - q1
+        lower = q1 - 1.5*iqr
+        upper = q3 + 1.5*iqr
+
+        b = np.logical_and(b, np.logical_or(l > lower , np.isnan(l)))
+        b = np.logical_and(b, np.logical_or(l < upper , np.isnan(l)))
+    
+    return b
+
+def norm(v):
+    nonnull = v[np.isnan(v) == False]
+    max = np.nanmax(nonnull)
+    min = np.nanmin(nonnull)
+
+    return 2*((v - min) / (max - min))-1
+
+def get_local_gaussian(ys, numbins=50):
+    max = np.nanmax(ys)
+    min = np.nanmin(ys)
+    bins = np.linspace(min, max, num=numbins)
+    s_ys = np.array(sorted(ys, reverse=True))
+
+    d, m, s = [], [], []
+    for i in range(numbins-1):
+        low = bins[i]
+        high = bins[i+1]
+        tbool = np.logical_and(low<=s_ys, s_ys<=high)
+        data = s_ys[tbool]
+        d.append(len(data))
+        m.append(data.mean() if not np.isnan(data.mean()) else low)
+        s.append(data.std() if not np.isnan(data.std()) else 0.01)
+    d = np.array(d) / sum(d)
+
+    return d, np.array(m), np.array(s)
+
+def convert_categorical(ys, numbins=50):
+    max = np.nanmax(ys)
+    min = np.nanmin(ys)
+    bins = np.linspace(min, max, num=numbins)
+    s = np.array(sorted(enumerate(ys), key=lambda x:x[1]))
+    s_inds = s[:,0].astype(int)
+    s_ys = s[:,1]
+    
+    
+    nys = deepcopy(ys)
+    for i in range(numbins-1):
+        low = bins[i]
+        high = bins[i+1]
+        tbool = np.logical_and(low<=s_ys, s_ys<=high)
+        
+        if sum(tbool) > 1:
+            nys[np.array(s_inds[tbool])] = i+1
+
+    print(i+1)
+    nys[np.isnan(nys)] = numbins
+    return nys
+
+def sample_local_gaussian(v, numbins=50, infilling=None):
+    d,m,s = get_local_gaussian(v, numbins=numbins)
+    num = sum(np.isnan(v))
+
+    samples = np.random.choice(numbins-1, num, p=d)
+    rand_n = np.random.randn(num)
+
+    adjust = m[samples] + 1.2 * rand_n *s[samples]
+    
+    # override 
+    #adjust = np.zeros(adjust.shape)
+    
+    if infilling == "zeros":
+        adjust = np.zeros(adjust.shape)
+
+    if infilling == "uniform":
+        adjust = np.random.uniform(low=-1.0, high=1.0, size=adjust.shape)
+
+    if infilling == "gaussian":
+        adjust = np.random.normal(size=adjust.shape)
+
+    v[np.isnan(v)] = adjust
+    return v, (d,m,s)
+
+
+def sample_noise(size, dmss, numbins=50, noise=None):
+    if noise == "uniform":
+        return np.random.uniform(low=-1.0, high=1.0, size=size)
+    
+    if noise == "gaussian":
+        return np.random.normal(size=size)
+
+    vs = []
+    for d,m,s in dmss:
+        samples = np.random.choice(numbins-1, size[0], p=d)
+        rand_n = np.random.randn(size[0])
+        vs.append(m[samples] + 1.2 * rand_n *s[samples])
+    return np.stack(vs, axis=-1)
+
+def unison_shuffled_copies(a, b):
+    #assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return np.array(a)[p], np.array(b)[p]
